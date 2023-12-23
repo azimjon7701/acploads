0a8d4506cfeb9ed3da93e775193f18d2bf8b71c0
azimjon7701 <trigger7701@gmail.com>
2023-12-23 17:54:00 +0500

    redis off

--- utils/authentication.py
+++ utils/authentication.py
@@ -1,15 +1,14 @@
 import contextvars
-import json
 
+import redis
 from django.utils import timezone
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 
 from account import models
-import redis
 
-REDIS_DB = 0
-REDIS_CONN = (redis.Redis(host='127.0.0.1', port=6379, db=REDIS_DB))
+# REDIS_DB = 0
+# REDIS_CONN = (redis.Redis(host='127.0.0.1', port=6379, db=REDIS_DB))
 
 current_user = contextvars.ContextVar('current_user', default=None)
 
@@ -23,30 +22,30 @@ def get_current_user():
 
 
 def manage_current_user(token: models.AuthToken) -> dict:
-    user_data_json = REDIS_CONN.get(token.key)
+    # user_data_json = REDIS_CONN.get(token.key)
+    #
+    # if user_data_json:
+    #     user_data = json.loads(user_data_json)
+    # else:
+    user: models.User = token.user
+    profile: models.Profile = user.profile
+    company: models.Company = profile.get_company()
 
-    if user_data_json:
-        user_data = json.loads(user_data_json)
-    else:
-        user: models.User = token.user
-        profile: models.Profile = user.profile
-        company: models.Company = profile.get_company()
+    user_data: dict = {
+        "id": user.id,
+        "entity_type": company.entity_type,
+        "last_name": user.last_name,
+        "first_name": user.first_name,
+        "email": user.email,
+        "phone": profile.phone,
+        "mc": company.mc,
+        "usdot": company.usdot,
+        "company_name": company.name,
+        "company_address": company.address1,
+        "company_phone": company.phone
 
-        user_data: dict = {
-            "id": user.id,
-            "entity_type": company.entity_type,
-            "last_name": user.last_name,
-            "first_name": user.first_name,
-            "email": user.email,
-            "phone": profile.phone,
-            "mc": company.mc,
-            "usdot": company.usdot,
-            "company_name": company.name,
-            "company_address": company.address1,
-            "company_phone": company.phone
-
-        }
-        REDIS_CONN.set(token.key, json.dumps(user_data))
+    }
+    # REDIS_CONN.set(token.key, json.dumps(user_data))
     __set_current_user(user_data)
     return user_data
 
