f5e414a1fab97de1a6e3b4f3d610519bedce2e24
azimjon7701 <trigger7701@gmail.com>
2024-01-08 11:14:09 +0500

    Load api

--- any_cap/settings.py
+++ any_cap/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-1fq$_v@3d)hlh5gd(r*%0f(t)l4osy59py31)=$=jw))96o(@y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'
+ALLOWED_HOSTS = ['*']
 
 
 CORS_ALLOWED_ORIGINS = [
